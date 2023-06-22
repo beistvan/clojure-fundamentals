deb5a6e6f276a419493d34fce8411c306ca3413b
Beregszászi István <beistvan@gmail.com>
2023-06-22 09:04:32 +0200

    Symbols

---
+++ symbols.clj
@@ -0,0 +1,20 @@
+;; Symbols
+
+(+ 1 2)
+
+;; Namespaced Symbols
+(clojure.core/+ 1 2)
+
+;; Special Symbols
+
+nil
+true
+false
+
+(println (+ 1 2)) ;;=> 3
+
+(println (clojure.core/+ 1 2)) ;;=> 3
+
+(println nil) ;;=> nil
+(println true) ;;=> true
+(println false) ;;=> false
